d5c78b22fda9912009a537f2ab8a2e0ff1b013a1
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 20:25:33 +0100

    test meal by id errotype RED

--- tests/test_mealById.py
+++ tests/test_mealById.py
@@ -8,10 +8,12 @@ class MealNameByIdTest(unittest.TestCase):
     def setUp(self):
         self.temp = MealById()
 
-    def test_meal_instruction(self):
+    def test_meal_name_By_id(self):
         mealById = self.temp.get_meal_name_by_id("52772")
         self.assertEqual(mealById[0]['strMeal'],"Teriyaki Chicken Casserole")
 
+    def test_meal_name_by_id_errorType(self):
+        self.assertRaisesRegex(TypeError, 'not str number', self.temp.get_meal_name_by_id, 1)
 
     def tearDown(self):
         self.temp = None
