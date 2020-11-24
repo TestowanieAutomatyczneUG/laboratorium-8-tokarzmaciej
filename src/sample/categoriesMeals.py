90ac535d5697bda51b8e9768fdf706de2cc978df
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 19:52:32 +0100

    test meal categories GREEN

--- src/sample/categoriesMeals.py
+++ src/sample/categoriesMeals.py
@@ -2,4 +2,9 @@ import requests
 
 class MealCategoris:
     def get_meal_categories(self):
-        return ""
+        categories = requests.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+        result = []
+        for category in categories.json()['categories']:
+            result.append(category['strCategory'])
+        return result
+
