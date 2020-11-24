6dc746789b1b98177c85a04d33bac2bed8304a29
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 21:45:30 +0100

    tests meals in category

--- src/sample/categoriesMeals.py
+++ src/sample/categoriesMeals.py
@@ -8,3 +8,18 @@ class MealCategoris:
         for category in categories.json()['categories']:
             result.append(category['strCategory'])
         return result
+
+    def get_meals_in_category(self, categoryName):
+        if type(categoryName) == str:
+            result = requests.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + categoryName)
+            meals = []
+            if result.json()['meals'] != None:
+                for category in result.json()['meals']:
+                    meals.append(category['strMeal'])
+                return meals
+            else:
+                return None
+        else:
+            raise TypeError("You_use_str_type")
+
+print(MealCategoris().get_meals_in_category("Seafood"))
--- tests/test_categoriesMeals.py
+++ tests/test_categoriesMeals.py
@@ -3,14 +3,30 @@ import unittest
 from src.sample.categoriesMeals import MealCategoris
 
 
-class MealNameTest(unittest.TestCase):
+class MealCategoriesTest(unittest.TestCase):
 
     def setUp(self):
         self.temp = MealCategoris()
 
     def test_meal_categories(self):
-        categories = ['Beef', 'Chicken', 'Dessert', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian', 'Breakfast', 'Goat']
-        self.assertEqual(self.temp.get_meal_categories(),categories)
+        categories = ['Beef', 'Chicken', 'Dessert', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side',
+                      'Starter', 'Vegan', 'Vegetarian', 'Breakfast', 'Goat']
+        self.assertEqual(self.temp.get_meal_categories(), categories)
+
+    def test_meals_in_category(self):
+        meals = ['Baked salmon with fennel & tomatoes', 'Cajun spiced fish tacos', 'Escovitch Fish', 'Fish pie',
+                 'Fish Stew with Rouille', 'Garides Saganaki', 'Honey Teriyaki Salmon', 'Kedgeree', 'Kung Po Prawns',
+                 'Laksa King Prawn Noodles', 'Mediterranean Pasta Salad', 'Recheado Masala Fish',
+                 'Salmon Avocado Salad', 'Salmon Prawn Risotto', 'Saltfish and Ackee', 'Seafood fideuà',
+                 'Shrimp Chow Fun', 'Sledz w Oleju (Polish Herrings)', 'Three Fish Pie', 'Tuna and Egg Briks',
+                 'Tuna Nicoise']
+        self.assertEqual(self.temp.get_meals_in_category("Seafood"), meals)
+
+    def test_meals_in_category_not_found(self):
+        self.assertEqual(self.temp.get_meals_in_category("none"), None)
+
+    def test_meals_in_categorya_type_error(self):
+        self.assertRaisesRegex(TypeError, "You_use_str_type", self.temp.get_meals_in_category, False)
 
     def tearDown(self):
         self.temp = None
