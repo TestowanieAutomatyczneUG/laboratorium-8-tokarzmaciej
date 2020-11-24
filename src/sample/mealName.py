1ba4f4814a84c7aab8fa733e1298c1da316d901e
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 18:06:10 +0100

    test meal instruction REFACTOR

--- src/sample/mealName.py
+++ src/sample/mealName.py
@@ -1,8 +1,7 @@
 import requests
+
+
 class MealName:
     def get_meal_instruction(self, name):
         meal = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + name)
         return meal.json()['meals']
-
-
-
