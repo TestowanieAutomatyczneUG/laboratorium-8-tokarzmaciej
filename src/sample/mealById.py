0309cfaf5e2c42b1c11f8d5bfad557ac7dec6869
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 20:10:40 +0100

    test meal by id REFACTOR

--- src/sample/mealById.py
+++ src/sample/mealById.py
@@ -2,8 +2,6 @@ import requests
 
 
 class MealById:
-    def get_meal_name_by_id(self,id):
+    def get_meal_name_by_id(self, id):
         meals = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
         return meals.json()['meals']
-
-
