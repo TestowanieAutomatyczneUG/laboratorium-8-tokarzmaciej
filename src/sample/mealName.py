41722102b7b2574eda7f80322ba26c3bae22d48b
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 18:52:26 +0100

    test meal instruction type GREEN

--- src/sample/mealName.py
+++ src/sample/mealName.py
@@ -3,5 +3,8 @@ import requests
 
 class MealName:
     def get_meal_instruction(self, name):
-        meal = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + name)
-        return meal.json()['meals']
+        if type(name)==str:
+            meal = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + name)
+            return meal.json()['meals']
+        else:
+            raise TypeError('not type string')
