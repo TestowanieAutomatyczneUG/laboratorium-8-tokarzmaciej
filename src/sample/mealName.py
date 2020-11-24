81fc27fa50e33d6a2214dab2cc6e940124cc4cc9
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 18:53:42 +0100

    test meal instruction type REFACTOR

--- src/sample/mealName.py
+++ src/sample/mealName.py
@@ -3,7 +3,7 @@ import requests
 
 class MealName:
     def get_meal_instruction(self, name):
-        if type(name)==str:
+        if type(name) == str:
             meal = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + name)
             return meal.json()['meals']
         else:
