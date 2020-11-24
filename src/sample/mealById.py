9cf894af8225457c53681943dab46afbfb3658cf
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 20:26:31 +0100

    test meal by id errotype GREEN

--- src/sample/mealById.py
+++ src/sample/mealById.py
@@ -3,5 +3,8 @@ import requests
 
 class MealById:
     def get_meal_name_by_id(self, id):
-        meals = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
-        return meals.json()['meals']
+        if type(id) == str:
+            meals = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+            return meals.json()['meals']
+        else:
+            raise TypeError("not str number")
