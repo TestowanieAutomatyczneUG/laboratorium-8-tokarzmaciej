ca8f737cc78bb189620d0566fd8fc376fea5c520
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 21:52:38 +0100

    test area

--- src/sample/areaMeals.py
+++ src/sample/areaMeals.py
@@ -2,6 +2,13 @@ import requests
 
 
 class AreaMeals:
+    def get_area(self):
+        categories = requests.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+        result = []
+        for category in categories.json()['meals']:
+            result.append(category['strArea'])
+        return result
+
     def get_meals_in_area(self, areaName):
         if type(areaName) == str:
             result = requests.get('https://www.themealdb.com/api/json/v1/1/filter.php?a=' + areaName)
--- tests/test_areaMeals.py
+++ tests/test_areaMeals.py
@@ -8,6 +8,12 @@ class AreaMealsTest(unittest.TestCase):
     def setUp(self):
         self.temp = AreaMeals()
 
+    def test_areas(self):
+        areas = ['American', 'British', 'Canadian', 'Chinese', 'Dutch', 'Egyptian', 'French', 'Greek', 'Indian',
+                 'Irish', 'Italian', 'Jamaican', 'Japanese', 'Kenyan', 'Malaysian', 'Mexican', 'Moroccan', 'Polish',
+                 'Russian', 'Spanish', 'Thai', 'Tunisian', 'Turkish', 'Unknown', 'Vietnamese']
+        self.assertEqual(self.temp.get_area(), areas)
+
     def test_meals_in_area(self):
         meals = ['BeaverTails', 'Breakfast Potatoes', 'Canadian Butter Tarts', 'Montreal Smoked Meat', 'Nanaimo Bars',
                  'Pate Chinois', 'Pouding chomeur', 'Poutine', 'Rappie Pie', 'Split Pea Soup', 'Sugar Pie', 'Timbits',
