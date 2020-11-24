8d44c2e22402bf48c846557a2530bbc7befa95cd
Maciej <maciej.tokarz1@wp.pl>
2020-11-24 20:08:23 +0100

    test meal by id GREEN

--- src/sample/mealById.py
+++ src/sample/mealById.py
@@ -3,6 +3,7 @@ import requests
 
 class MealById:
     def get_meal_name_by_id(self,id):
-        return ""
+        meals = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+        return meals.json()['meals']
 
 
